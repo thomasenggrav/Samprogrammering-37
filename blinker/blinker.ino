0f23ef5bbb8c806e9b61e228f2390683effbbf39
thomasenggrav <thomas.enggrav2@icloud.com>
2023-09-14 15:24:29 +0200

    Adds Led

--- blinker/blinker.ino
+++ blinker/blinker.ino
@@ -1,10 +1,20 @@
+const int ledPin = 13;
+
+void blinkLed()
+{
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);
+}
+
 void setup()
 {
+    pinMode(ledPin, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop()
 {
-    Serial.println("B");
-    delay(1000);
+    blinkLed();   
 }
